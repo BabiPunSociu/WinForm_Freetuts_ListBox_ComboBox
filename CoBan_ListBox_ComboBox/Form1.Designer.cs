fb8552894557210810ae609fdde6c76bc83eb34b
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 01:12:25 +0700

    DungNguyen

---
+++ CoBan_ListBox_ComboBox/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace CoBan_ListBox_ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnXoaCuoi = new System.Windows.Forms.Button();
+            this.btnXoaDau = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstKetQua = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightPink;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(800, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(209, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KHAI BÁO Y TẾ ĐIỆN TỬ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtHoTen);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox2.Location = new System.Drawing.Point(0, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(241, 375);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtHoTen.Location = new System.Drawing.Point(3, 47);
+            this.txtHoTen.Multiline = true;
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(235, 284);
+            this.txtHoTen.TabIndex = 3;
+            this.txtHoTen.Enter += new System.EventHandler(this.txtHoTen_Enter);
+            this.txtHoTen.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHoTen_KeyPress);
+            this.txtHoTen.Leave += new System.EventHandler(this.txtHoTen_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(3, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Nhập thông tin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Họ Tên:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox4);
+            this.groupBox3.Controls.Add(this.lstKetQua);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(241, 75);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(559, 375);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnClear);
+            this.groupBox4.Controls.Add(this.btnXoaCuoi);
+            this.groupBox4.Controls.Add(this.btnXoaDau);
+            this.groupBox4.Controls.Add(this.btnXoa);
+            this.groupBox4.Controls.Add(this.txtSoLuong);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox4.Location = new System.Drawing.Point(3, 236);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(553, 136);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(301, 95);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(246, 31);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Xóa toàn bộ thông tin";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            this.btnClear.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.btnClear.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // btnXoaCuoi
+            // 
+            this.btnXoaCuoi.Location = new System.Drawing.Point(13, 95);
+            this.btnXoaCuoi.Name = "btnXoaCuoi";
+            this.btnXoaCuoi.Size = new System.Drawing.Size(246, 31);
+            this.btnXoaCuoi.TabIndex = 4;
+            this.btnXoaCuoi.Text = "Xóa thông tin cuối cùng";
+            this.btnXoaCuoi.UseVisualStyleBackColor = true;
+            this.btnXoaCuoi.Click += new System.EventHandler(this.btnXoaCuoi_Click);
+            this.btnXoaCuoi.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.btnXoaCuoi.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // btnXoaDau
+            // 
+            this.btnXoaDau.Location = new System.Drawing.Point(301, 49);
+            this.btnXoaDau.Name = "btnXoaDau";
+            this.btnXoaDau.Size = new System.Drawing.Size(246, 31);
+            this.btnXoaDau.TabIndex = 3;
+            this.btnXoaDau.Text = "Xóa thông tin đầu tiên";
+            this.btnXoaDau.UseVisualStyleBackColor = true;
+            this.btnXoaDau.Click += new System.EventHandler(this.btnXoaDau_Click);
+            this.btnXoaDau.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.btnXoaDau.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(13, 49);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(246, 31);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa thông tin đang chọn";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            this.btnXoa.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.btnXoa.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(236, 18);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.ReadOnly = true;
+            this.txtSoLuong.Size = new System.Drawing.Size(316, 22);
+            this.txtSoLuong.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(223, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Số người đã khai báo:";
+            // 
+            // lstKetQua
+            // 
+            this.lstKetQua.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lstKetQua.FormattingEnabled = true;
+            this.lstKetQua.ItemHeight = 16;
+            this.lstKetQua.Location = new System.Drawing.Point(3, 18);
+            this.lstKetQua.Name = "lstKetQua";
+            this.lstKetQua.Size = new System.Drawing.Size(553, 212);
+            this.lstKetQua.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ListBox ComboBox";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstKetQua;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnXoaCuoi;
+        private System.Windows.Forms.Button btnXoaDau;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
+
---
+++ CoBan_ListBox_ComboBox/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoBan_ListBox_ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHoTen_Enter(object sender, EventArgs e)
+        {
+            txtHoTen.BackColor = Color.LightPink;
+        }
+
+        private void txtHoTen_Leave(object sender, EventArgs e)
+        {
+            txtHoTen.BackColor = Color.White;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra rỗng
+            if (txtHoTen.Text.Trim() == "")
+            {
+                MessageBox.Show("Bạn cần nhập họ tên");
+                txtHoTen.Focus();
+                return;
+            }
+
+            // Kiểm tra Item đã tồn tại
+            foreach (string item in lstKetQua.Items)
+            {
+                if (item.Trim() == txtHoTen.Text.Trim())
+                {
+                    MessageBox.Show(txtHoTen.Text.Trim() + " đã tồn tại");
+                    txtHoTen.Text = "";
+                    txtHoTen.Focus();
+                    return;
+                }
+            }
+
+            // Item chưa tồn tại -> Add Item vào listBox
+            lstKetQua.Items.Add(txtHoTen.Text.Trim());
+            txtHoTen.Text = "";
+            txtHoTen.Focus();
+            txtSoLuong.Text = lstKetQua.Items.Count.ToString();
+        }
+
+        
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.LightPink;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.White;
+        }
+
+        private void txtHoTen_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)  // ENTER
+            {
+                e.Handled = true;
+                // Kiểm tra rỗng
+                if (txtHoTen.Text.Trim() == "")
+                {
+                    MessageBox.Show("Bạn cần nhập họ tên");
+                    txtHoTen.Focus();
+                    return;
+                }
+
+                // Kiểm tra Item đã tồn tại
+                foreach (string item in lstKetQua.Items)
+                {
+                    if(item.Trim() == txtHoTen.Text.Trim())
+                    {
+                        MessageBox.Show(txtHoTen.Text.Trim() + " đã tồn tại");
+                        txtHoTen.Text = "";
+                        txtHoTen.Focus();
+                        return;
+                    }    
+                }    
+
+                // Item chưa tồn tại -> Add Item vào listBox
+                lstKetQua.Items.Add(txtHoTen.Text.Trim());
+                txtHoTen.Text = "";
+                txtHoTen.Focus();
+                txtSoLuong.Text = lstKetQua.Items.Count.ToString();
+            }    
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (lstKetQua.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Bạn chưa chọn phần tử nào");
+                return;
+            }
+            int index = lstKetQua.SelectedIndex;
+            if (MessageBox.Show("Bạn chắc chắn muốn xóa " + lstKetQua.Items[index] + " không?",
+                "Cảnh báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                lstKetQua.Items.RemoveAt(index);
+            }
+            txtSoLuong.Text = lstKetQua.Items.Count.ToString();
+        }
+
+        private void btnXoaDau_Click(object sender, EventArgs e)
+        {
+            if(lstKetQua.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Chưa có dữ liệu");
+                return;
+            }    
+            else
+            {
+                if (MessageBox.Show("Bạn chắc chắn muốn xóa " + lstKetQua.Items[0] + " không?",
+                    "Cảnh báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    lstKetQua.Items.RemoveAt(0);
+                }
+            }
+            txtSoLuong.Text = lstKetQua.Items.Count.ToString();
+        }
+
+        private void btnXoaCuoi_Click(object sender, EventArgs e)
+        {
+            if (lstKetQua.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Chưa có dữ liệu");
+                return;
+            }
+            else
+            {
+                int index = lstKetQua.Items.Count - 1;
+                if (MessageBox.Show("Bạn chắc chắn muốn xóa " + lstKetQua.Items[index] + " không?",
+                    "Cảnh báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    lstKetQua.Items.RemoveAt(index);
+                }
+            }
+            txtSoLuong.Text = lstKetQua.Items.Count.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstKetQua.Items.Clear();
+            txtSoLuong.Text = lstKetQua.Items.Count.ToString();
+        }
+    }
+}
